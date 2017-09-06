b39c287d8b3d0b368d91f8b45fcd02594a9949cd
qichong <894556569@qq.com>
2017-09-06 10:33:12 +0800

    no message

--- mySafeKeyboardDemo/ISTSafeKeyBoard/Utils/UIResponder+ISTSafeKeyboardFirstResponder.m
+++ mySafeKeyboardDemo/ISTSafeKeyBoard/Utils/UIResponder+ISTSafeKeyboardFirstResponder.m
@@ -12,6 +12,12 @@ static __weak id safe_currentFirstResponder;
 @implementation UIResponder (ISTSafeKeyboardFirstResponder)
 + (id)safe_currentFirstResponder{
     safe_currentFirstResponder = nil;
+    /*
+     UIWindow keyWindow = [[UIApplication sharedApplication] keyWindow];
+     UIView firstResponder = [keyWindow performSelector:@selector(firstResponder)];
+     */
+    //以上方法调用了私有api，审核会被拒绝， 用递归也可以找到第一响应者，但是有点呵呵
+    
     // 通过将target设置为nil，让系统自动遍历响应链
     // 从而响应链当前第一响应者响应我们自定义的方法
     [[UIApplication sharedApplication] sendAction:@selector(safe_findFirstResponder:)
